923a37f4eb59145c52f0a41f689391f16dbecca2
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2023-05-14 16:55:08 -0300

    projeto para tela de mobile completo

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "gray-text": "#656565",
+        "orange-btn": "#EE7C0E",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+      fontSize: {
+        29: "29px",
+        33: "33px",
+      },
+      maxWidth: {
+        1140: "1140px",
+      },
+    },
+  },
+  plugins: [],
+};
